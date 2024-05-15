66644dc2c7aabb5e3d0a5c58a3c7e8d41e274244
Andrea Leardini <andre8244@gmail.com>
2024-05-15 12:06:25 +0200

    Save work/relax minutes on local storage

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import { useTitle } from '@vueuse/core'
 import { NeButton, NeTextInput } from '@nethesis/vue-components'
 
 const windowTitle = useTitle()
 
 const appName = 'vue-timer'
-const workMinutes = ref(25)
-const relaxMinutes = ref(5)
+const defaultWorkMinsString = '30'
+const defaultRelaxMinsString = '5'
+const workMinutes = ref(defaultWorkMinsString)
+const relaxMinutes = ref(defaultRelaxMinsString)
 const countdown = ref(0)
 const timerWorker = new Worker('src/timerWorker.ts')
 windowTitle.value = appName
@@ -21,13 +23,18 @@ const formattedCountdown = computed(() => {
 
 const minutesRemaining = computed(() => Math.floor(countdown.value / 60))
 
+onMounted(() => {
+  workMinutes.value = localStorage.getItem('workMinutes') || defaultWorkMinsString
+  relaxMinutes.value = localStorage.getItem('relaxMinutes') || defaultRelaxMinsString
+})
+
 const startWork = () => {
-  countdown.value = workMinutes.value * 60
+  countdown.value = Number(workMinutes.value) * 60
   startTimer()
 }
 
 const startRelax = () => {
-  countdown.value = relaxMinutes.value * 60
+  countdown.value = Number(relaxMinutes.value) * 60
   startTimer()
 }
 
@@ -54,19 +61,49 @@ const startTimer = () => {
     }
   }
 }
+
+function onInputWorkMinutes(e: InputEvent) {
+  const target = e.target as HTMLInputElement
+  const value = Number(target.value)
+
+  if (value > 0) {
+    localStorage.setItem('workMinutes', value.toString())
+  }
+}
+
+function onInputRelaxMinutes(e: InputEvent) {
+  const target = e.target as HTMLInputElement
+  const value = Number(target.value)
+
+  if (value > 0) {
+    localStorage.setItem('relaxMinutes', value.toString())
+  }
+}
 </script>
 
 <template>
-  <div class="p-6 space-y-6 max-w-2xl m-auto">
-    <div class="text-9xl text-center text-primary-700 dark:text-primary-300">
+  <div class="p-8 space-y-6 max-w-2xl m-auto">
+    <div class="text-9xl text-center text-primary-700 dark:text-primary-300 mb-12">
       {{ formattedCountdown }}
     </div>
     <form @submit.prevent="startWork" class="flex justify-center gap-4 items-start">
-      <NeTextInput type="number" v-model="workMinutes" class="w-40" helperText="Minutes" />
+      <NeTextInput
+        type="number"
+        v-model="workMinutes"
+        class="w-40"
+        helperText="Minutes"
+        @input="onInputWorkMinutes"
+      />
       <NeButton type="submit" kind="primary" size="lg">Start work</NeButton>
     </form>
     <form @submit.prevent="startRelax" class="flex justify-center gap-4 items-start">
-      <NeTextInput type="number" v-model="relaxMinutes" class="w-40" helperText="Minutes" />
+      <NeTextInput
+        type="number"
+        v-model="relaxMinutes"
+        class="w-40"
+        helperText="Minutes"
+        @input="onInputRelaxMinutes"
+      />
       <NeButton type="submit" kind="primary" size="lg">Start relax</NeButton>
     </form>
   </div>
